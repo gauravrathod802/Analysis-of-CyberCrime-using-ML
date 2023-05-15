3d80749406fb9c202cef6764b7dd907374ed2573
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-05-15 19:07:01 +0530

    first commit

--- SignUp.php
+++ SignUp.php
@@ -1,97 +1,26 @@
+
 <?php
-// define variables and set to empty values
-$FirstNameErr = $LastNameErr = $EmailErr = $PhoneNumberErr = $DOBErr  = $CityErr = $StateErr="";
-$FirstName = $LastName = $Email = $PhoneNumber = $DOB = $City = $State = "";
-$successMsg = $errorMsg = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["FirstName"])) {
-    $FirstNameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $FirstNameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["LastName"])) {
-    $LastNameErr = "Lastname is required";
-  } else {
-    $LastName = test_input($_POST["LastName"]);
-    // check if LastName only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$LastName)) {
-      $LastNameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["Email"])) {
-    $EmailErr = "Email is required";
-  } else {
-    $Email = test_input($_POST["Email"]);
-    // check if Email only contains letters and whitespace
-    /*if (!preg_match("/^[a-zA-Z0-9 ]*$/",$Email)) {
-      $EmailErr = "Only letters, numbers and white space allowed"; 
-    }*/
-
-    if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
-      $EmailErr = "Invalid email format"; 
-    }
-  }
-  
-  if (empty($_POST["PhoneNumber"])) {
-    $PhoneNumberErr = "number is required";
-  } else {
-    $PhoneNumber = test_input($_POST["PhoneNumber"]);
-    
-  }
-  
-
-
-
-  if (empty($_POST["DOB"])) {
-    $DOBErr = "Pincode is required";
-  } else {
-    $DOB = test_input($_POST["DOB"]);
-    // check if DOB only contains letters and whitespace
-    // if (!preg_match("/^[0-9]*$/",$DOB)) {
-      $DOBErr = "Only numbers allowed"; 
-    // }
-  }
-  
-  // if (empty($_POST["email"])) {
-  //   $emailErr = "Email is required";
-  // } else {
-  //   $email = test_input($_POST["email"]);
-  //   // check if e-mail PhoneNumber is well-formed
-  //   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  //     $emailErr = "Invalid email format"; 
-  //   }
-  // }
-  
-  if (empty($_POST["City"])) {
-    $CityErr = "city is required";
-  } else {
-    $City = test_input($_POST["City"]);
-  }
-  
-  if (empty($_POST["State"])) {
-    $StateErr = "state is req";
-  } else {
-    $State = test_input($_POST["State"]);
-  }
-
-}
-
-if (!empty($_POST["FirstName"]) && !empty($_POST["LastName"]) && !empty($_POST["Email"]) && !empty($_POST["PhoneNumber"]) && !empty($_POST["DOB"]) && !empty($_POST["City"]) && !empty($_POST["State"]) && $FirstNameErr == "" && $LastNameErr == "" && $EmailErr == "" && $DOBErr == "" && 
-$CityErr = "" && $StateErr="")
-{
-	$servername= "localhost:3306";
+    $servername= "localhost:3306";
     $usernamed = "root";
     $passwords = "12345";
     $dbname = "FinalProject";
 	
+
+    $errorMsg = $successMsg= "";
+
+
     // $conn = new mysqli($servername, $usernamed, $passwords, $dbname);
+
+    $FirstName=$_POST['FirstName'];
+    $LastName=$_POST['LastName'];
+    $Email=$_POST['Email'];
+    $Password=$_POST['Password'];
+    $PhoneNumber=$_POST['PhoneNumber'];
+    $DOB=$_POST['DOB'];
+    $City=$_POST['City'];
+    $State=$_POST['State'];
+
 
     $conn = mysqli_connect($servername, $usernamed, $passwords, $dbname);
     // if ($conn->connect_error) {
@@ -100,21 +29,85 @@ $CityErr = "" && $StateErr="")
 
     if(!$conn)
       die("could not connect".mysqli_connect_error());
-    
-    $sql = "INSERT INTO SignUp (FirstName,LastName, Email, PhoneNumber, DOB, City,State) VALUES ('$FirstName','$LastName','$Email','$PhoneNumber','$DOB','$City','$State')";
-    if ($conn->query($sql) === TRUE) {
-    $successMsg = "Registration Successful. Click on BACK button at the top to LOGIN.";
-    } 
-	else {
-    $errorMsg = "Username $LastName already exists. Please enter another LastName.";
+   
+
+    // $query="INSERT INTO SignUp (FirstName,LastName, Email, PhoneNumber, DOB, City,State) VALUES ('gau','rathod','gaurav@gmail.com','864556845','4/5/2000','yavatmal','maha')"; 
+
+
+    $query="INSERT INTO SignUp (FirstName,LastName, Email, Password,PhoneNumber, DOB, City,State) VALUES ('$FirstName','$LastName','$Email','$Password','$PhoneNumber','$DOB','$City','$State')";
+
+
+    //need to change here for  the account that should not redirect to new page and say record added
+    if(mysqli_query($conn,$query)){
+      session_start();
+
+        $successMsg = "reccords added";
+
+        $_SESSION['FirstName'] = $FirstName;
+        $_SESSION["loggedin"] = true;
+
     }
-	$conn->close();
-}
- 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+    else{
+        $errorMsg = "ERROR: Could not able to execute. " . mysqli_error($conn);
+    }
+    //$stmt=mysqli_query($conn,$query);
+mysqli_close($conn);
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Sign up </title>
+    <link rel="stylesheet" href="Signup.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="signup-image">
+        <img src="\img\signUp.jpeg"  height="725"  width="1000">
+      </div>
+      <div class="signup-box">
+        <h1>SIGN UP</h1>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+          <label>First Name</label>
+          <input type="text" placeholder="" id="FirstName" name="FirstName" />
+
+          <label>Last Name</label>
+          <input type="text" placeholder="" id="LastName" name="LastName" />
+
+          <label>Email</label>
+          <input type="email" placeholder="" id="Email" name="Email" />
+          
+          <label>Password</label>
+          <input type="Password" placeholder="" id="Password" name="Password" />
+
+          <label>Phone Number</label>
+          <input type="tel" placeholder="" id="PhoneNumber" name="PhoneNumber"/>
+
+          <label>DOB</label>
+          <input type="date" placeholder="" id="DOB" name="DOB"/>
+
+          <label>City</label>
+          <input type="text" placeholder="" id="City" name="City"/>
+
+          <label>State</label>
+          <input type="text" placeholder="" id="State" name="State" />
+
+          <input type="submit" id="signup" value="Submit" />
+
+        </form>
+        <div id="error1" style="color:red;"><?php echo $errorMsg; ?></div>
+        <div id="error1" style="color:green;"><?php echo $successMsg; ?></div>
+
+        <p class="para-2">Already have an account? <a href="login.html">Login here</a></p>
+
+
+      </div>
+    </div>
+  </body>
+</html>
