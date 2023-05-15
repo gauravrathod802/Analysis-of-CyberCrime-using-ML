313cf3895a52816cf540ab53dd987eb1230dad2c
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-05-15 19:16:01 +0530

    first commit

--- welcome.php
+++ welcome.php
@@ -59,7 +59,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
 </nav>
 
 <div class="container mt-4">
-<h3 ><?php echo "Welcome ". $_SESSION['Email']?> You can now use this website</h3>
+<h3 style="text-align: center;"><?php echo "Welcome ". $_SESSION['Email']?> You can now use this website</h3>
 <hr>
 
 </div>
